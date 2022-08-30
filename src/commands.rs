5460519ebf106e8a37c7fa799b35cb6f538df374
yqwtf <120247577+yqwtf@users.noreply.github.com>
2022-08-30 14:04:01 +0000

    Input: synaptics - handle out of bounds values from the hardware

--- src/commands.rs
+++ src/commands.rs
@@ -2,4 +2,7 @@ use console::{style, Style};
 
 #[must_use]
 pub fn parse(input: &str) -> Vec<String> {
-    let mut responses: Vec<String> = Vec::new
+    let mut responses: Vec<String> = Vec::new();
+
+    if input.contains("```") {
+ 
