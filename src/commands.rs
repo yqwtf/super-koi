657c691559da212cfb6b2c1facf216b5622fcf6a
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-03-08 22:52:24 +0000

    new tags

--- src/commands.rs
+++ src/commands.rs
@@ -80,4 +80,5 @@ pub fn run_checked(command: String) -> String {
             "Don't Run This Command" => {
                 return error_style.apply_to("Command Run Cancelled\n").to_string();
             }
-           
+            "Don't Ask For This Response" => {
+    
