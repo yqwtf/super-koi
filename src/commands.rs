63f718dff8be634266ea31464666b398df639b15
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-04-01 08:48:05 +0000

    Version 1.4.3

--- src/commands.rs
+++ src/commands.rs
@@ -97,4 +97,6 @@ pub fn run_checked(command: String) -> String {
     let output = std::process::Command::new("sh")
         .arg("-c")
         .arg(command)
-      
+        .output();
+
+    match output {
