37781d16eb749120261524f8292380d4979040fc
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-04-06 13:25:02 +0000

    dirty

--- src/commands.rs
+++ src/commands.rs
@@ -101,4 +101,5 @@ pub fn run_checked(command: String) -> String {
 
     match output {
         Ok(output) => String::from_utf8_lossy(&output.stdout).to_string(),
-        Err(err) => error_s
+        Err(err) => error_style
+            .apply_to(format!("
