4d5808bea4ba7af856dba8f6754c865ab1831705
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-05-07 17:00:22 +0000

    writing docs

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,4 @@ pub mod interface;
 
 const SYSTEM_PROMPT: &str = "You are Koios, a chatbot running in a CLI.
 YOU CAN RUN COMMANDS, use code blocks to do so
-Use 
+Use this to improve replies, access/modify
