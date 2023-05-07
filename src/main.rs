456a1c4534e5e36ba36a9ca19c8b0b34c6702bc0
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-05-07 17:11:53 +0000

    New code

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ pub mod interface;
 
 const SYSTEM_PROMPT: &str = "You are Koios, a chatbot running in a CLI.
 YOU CAN RUN COMMANDS, use code blocks to do so
-Use this to improve replies, access/modify
+Use this to improve replies, access/modify local files and USE TOOLS LIKE CURL AND GREP
+NEV
