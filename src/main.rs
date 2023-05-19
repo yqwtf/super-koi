db4b4d7251fd538677fdb23cd5555af9a09896b2
yqwtf <120247577+yqwtf@users.noreply.github.com>
2023-05-19 15:41:53 +0000

    initial configuration

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,6 @@ const SYSTEM_PROMPT: &str = "You are Koios, a chatbot running in a CLI.
 YOU CAN RUN COMMANDS, use code blocks to do so
 Use this to improve replies, access/modify local files and USE TOOLS LIKE CURL AND GREP
 NEVER SAY YOU CAN'T ANSWER try your best
-you only have access to the shell DON'
+you only have access to the shell DON'T USE OTHER LANGUAGES
+
+User: What's 
